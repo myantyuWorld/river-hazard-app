f4299845f4b372cf6349377caf655190a983043b
yuichi.oba <yuichi.oba@sorich.jp>
2022-09-19 17:59:50 +0900

    modify

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,52 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+	<div class="container-md">
+		<div class="row mt-5">
+			<h1>河川監視</h1>
+		</div>
+		<div class="row mt-5">
+			<div class="row">
+				<div class="col-md-6"><img src="https://www.cgr.mlit.go.jp/cctv/ootagawa/61/cctv.jpg" /></div>
+			</div>
+			<div class="row">
+				<div class="col-md-6"><img src="https://www.cgr.mlit.go.jp/cctv/ootagawa/62/cctv.jpg" /></div>
+			</div>
+			<div class="row">
+				<div class="col-md-6"><img src="https://cam.river.go.jp/cam/now/122333161.jpg" /></div>
+			</div>
+			<div class="row">
+				<div class="col-md-6"><img src="https://cam.river.go.jp/cam/now/122333113.jpg" /></div>
+			</div>
+		</div>
+		<!-- <div class="row">
+			<Chart :size="{ width: 500, height: 400 }" :data="data" :margin="margin" :direction="direction">
+				<template #layers>
+					<Grid strokeDasharray="2,2" />
+					<Line :dataKeys="['name', 'pl']" />
+				</template>
+			</Chart>
+		</div> -->
+	</div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+import { defineComponent, ref } from 'vue'
+import { Chart, Grid, Line } from 'vue3-charts'
+import { plByMonth } from '@/data'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+export default defineComponent({
+	name: 'LineChart',
+	components: { Chart, Grid, Line },
+	setup() {
+		const data = ref(plByMonth)
+		const direction = ref('horizontal')
+		const margin = ref({
+			left: 0,
+			top: 20,
+			right: 20,
+			bottom: 0
+		})
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+		return { data, direction, margin }
+	}
+})
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css' // add
+import 'bootstrap-vue/dist/bootstrap-vue.css' // add
 
 import './assets/main.css'
-
 createApp(App).mount('#app')
